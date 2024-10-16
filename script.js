b441a5b8c95fa76e637b964ad45429a7c9acc33f
Muaz Muhammad <muazmuhammad21@gmail.com>
2024-10-16 13:26:44 +0600

    js update

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll for internal links
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800);
+    });
+});
